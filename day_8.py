389cea0937d1651052528646b2d97dad8b5a2ec0
Kornel Łaba <kornellaba44@gmail.com>
2026-04-23 18:10:56 +0200

    Refactor day_8.py to improve output formatting and calculation order for Apple and Tesla stock data
    Improves stock data output and calculation order
    
    Reorders calculations for clarity and adds descriptive prints of mean and volatility for both stocks. Enhances readability of correlation output and comments out unnecessary length print statements. Improves data analysis workflow and output clarity.

--- day_8.py
+++ day_8.py
@@ -6,12 +6,22 @@ apple_data = yf.download("AAPL", start="2023-01-01", end="2024-01-01")
 tesla_data = yf.download("TSLA", start="2023-01-01", end="2024-01-01")
 
 apple_data["Zwrot %"] = apple_data["Close"].pct_change() * 100
-tesla_data["Zwrot %"] = tesla_data["Close"].pct_change() * 100
-
 apple_data["SMA10"] = apple_data["Close"].rolling(10).mean()
-tesla_data["SMA10"] = tesla_data["Close"].rolling(10).mean()
+apple_mean = apple_data["Close"].mean()
+apple_std = apple_data["Close"].std()
 
-print(len(apple_data["Close"]), len(tesla_data["Close"]))
+print("\nŚrednia", apple_mean,
+      "\nZmienność", apple_std)
+
+tesla_data["Zwrot %"] = tesla_data["Close"].pct_change() * 100
+tesla_data["SMA10"] = tesla_data["Close"].rolling(10).mean()
+tesla_mean = tesla_data["Close"].mean()
+tesla_std = tesla_data["Close"].std()
+
+print("\nŚrednia", tesla_mean,
+      "\nZmienność", tesla_std)
+
+#print(len(apple_data["Close"]), len(tesla_data["Close"]))
 
 plt.plot(apple_data["Close"], label="Apple", color="#181BE4")
 plt.plot(apple_data["SMA10"], color="#18B4E4")
@@ -24,7 +34,7 @@ plt.legend()
 plt.show()
 
 corr = np.corrcoef(apple_data["Zwrot %"].dropna(), tesla_data["Zwrot %"].dropna())
-print(corr)
+print("\nkorelacja pomiędzy apple i tesla \n", corr)
 
 '''
 print(apple_data.head())
